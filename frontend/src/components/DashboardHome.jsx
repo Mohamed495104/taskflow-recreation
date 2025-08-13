3744bfe18f9dcd7c61a03baca95209c4291a1a15
Mohamed Ijas <71814248+Mohamed495104@users.noreply.github.com>
2025-08-13 14:58:07 -0400

    Update DashboardHome.jsx
    Update URL

--- frontend/src/components/DashboardHome.jsx
+++ frontend/src/components/DashboardHome.jsx
@@ -4,7 +4,7 @@ import { useAuth } from '../contexts/AuthContext';
 import { Link } from 'react-router-dom';
 import { request, gql } from 'graphql-request';
 
-const endpoint = process.env.REACT_APP_API_URL || 'https://taskflow-recreation.vercel.app/graphql';
+const endpoint = process.env.REACT_APP_API_URL || 'https://taskflow-recreation-jm9j.vercel.app/graphql';
 
 const GET_TASKS = gql`
   query Tasks($status: String, $searchTerm: String) {
